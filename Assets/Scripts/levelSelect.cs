4570dc98be0271340cce6c0dc3de7c61800b9048
wutperson <68948211+wutperson@users.noreply.github.com>
2021-03-17 13:32:15 +0300

    h

--- Assets/Scripts/levelSelect.cs
+++ Assets/Scripts/levelSelect.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class levelSelect : MonoBehaviour
 {
+    public Button[] buttons = new Button[12];
+
+    private void Start() {
+        int level = PlayerPrefs.GetInt("level", 1);
+
+        for(int i = 11; i > level; i = i - 1) {
+            buttons[i].enabled = false;
+        }
+    }
+
     public void onLevelSelect(Button button) {
         SceneManager.LoadScene(button.name);
     }
